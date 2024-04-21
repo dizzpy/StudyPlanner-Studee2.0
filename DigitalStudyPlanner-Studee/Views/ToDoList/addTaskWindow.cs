1d9f2456f63a24eba4c3bdb197d008c89ea700c5
Kawya_Ranmini <135610219+mtkranmini@users.noreply.github.com>
2024-04-22 00:15:51 +0530

    Note Main Page Done
    Note Main UserControl Done

--- DigitalStudyPlanner-Studee/Views/ToDoList/addTaskWindow.cs
+++ DigitalStudyPlanner-Studee/Views/ToDoList/addTaskWindow.cs
@@ -33,7 +33,7 @@ namespace DigitalStudyPlanner_Studee.Views.UserControlViews
         {
             // Set up Firestore with your project ID
             string projectId = "tsetingsampletodo";
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\chenu\\OneDrive\\Documents\\git\\StudyPlanner-Studee2.0\\DigitalStudyPlanner-Studee\\firecred.json");
+            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", "C:\\Users\\akila\\Desktop\\Studee-Files-New-Final\\Final_1.3\\StudyPlanner-Studee2.0\\DigitalStudyPlanner-Studee\\firecred.json");
             db = FirestoreDb.Create(projectId);
         }
 
--- DigitalStudyPlanner-Studee/Views/UserControlViews/UserNoteLib.Designer.cs
+++ DigitalStudyPlanner-Studee/Views/UserControlViews/UserNoteLib.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.label_hello = new System.Windows.Forms.Label();
+            this.AddNoteBtn = new Guna.UI.WinForms.GunaButton();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
             // 
             // label_hello
@@ -43,11 +45,46 @@
             this.label_hello.TabIndex = 2;
             this.label_hello.Text = "Note Library";
             // 
+            // AddNoteBtn
+            // 
+            this.AddNoteBtn.AnimationHoverSpeed = 0.07F;
+            this.AddNoteBtn.AnimationSpeed = 0.03F;
+            this.AddNoteBtn.BackColor = System.Drawing.Color.Transparent;
+            this.AddNoteBtn.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(156)))), ((int)(((byte)(116)))));
+            this.AddNoteBtn.BorderColor = System.Drawing.Color.Black;
+            this.AddNoteBtn.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.AddNoteBtn.FocusedColor = System.Drawing.Color.Empty;
+            this.AddNoteBtn.Font = new System.Drawing.Font("Poppins SemiBold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddNoteBtn.ForeColor = System.Drawing.Color.White;
+            this.AddNoteBtn.Image = null;
+            this.AddNoteBtn.ImageSize = new System.Drawing.Size(20, 20);
+            this.AddNoteBtn.Location = new System.Drawing.Point(736, 48);
+            this.AddNoteBtn.Name = "AddNoteBtn";
+            this.AddNoteBtn.OnHoverBaseColor = System.Drawing.Color.White;
+            this.AddNoteBtn.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.AddNoteBtn.OnHoverForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(156)))), ((int)(((byte)(116)))));
+            this.AddNoteBtn.OnHoverImage = null;
+            this.AddNoteBtn.OnPressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.AddNoteBtn.Radius = 10;
+            this.AddNoteBtn.Size = new System.Drawing.Size(181, 52);
+            this.AddNoteBtn.TabIndex = 3;
+            this.AddNoteBtn.Text = "Add Note";
+            this.AddNoteBtn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(39, 118);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(878, 563);
+            this.flowLayoutPanel1.TabIndex = 4;
+            // 
             // UserNoteLib
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.AddNoteBtn);
             this.Controls.Add(this.label_hello);
             this.Font = new System.Drawing.Font("Poppins", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
@@ -61,5 +98,7 @@
         #endregion
 
         private System.Windows.Forms.Label label_hello;
+        private Guna.UI.WinForms.GunaButton AddNoteBtn;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
