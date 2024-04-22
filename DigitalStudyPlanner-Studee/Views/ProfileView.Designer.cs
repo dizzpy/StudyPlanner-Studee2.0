51048b5d1ce5ba50be449714ab16bfe89dd3a7e1
Anuja Nisal Yashohara <rmanuja2003@gmail.com>
2024-04-22 15:09:43 +0530

    Profile data update function ( testing )

--- DigitalStudyPlanner-Studee/Views/ProfileView.Designer.cs
+++ DigitalStudyPlanner-Studee/Views/ProfileView.Designer.cs
@@ -29,13 +29,14 @@
         private void InitializeComponent()
         {
             this.ProfilePicBox = new Guna.UI.WinForms.GunaCirclePictureBox();
-            this.DOccupation = new System.Windows.Forms.Label();
             this.lblEmail = new System.Windows.Forms.Label();
             this.DEmailAddress = new System.Windows.Forms.Label();
-            this.DFullName = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.LogoutButton = new Guna.UI2.WinForms.Guna2Button();
+            this.DFullName = new Guna.UI2.WinForms.Guna2TextBox();
+            this.DOccupation = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SaveButton = new Guna.UI2.WinForms.Guna2Button();
             ((System.ComponentModel.ISupportInitialize)(this.ProfilePicBox)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,22 +53,6 @@
             this.ProfilePicBox.TabIndex = 12;
             this.ProfilePicBox.TabStop = false;
             this.ProfilePicBox.UseTransfarantBackground = true;
-            // 
-            // DOccupation
-            // 
-            this.DOccupation.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DOccupation.AutoSize = true;
-            this.DOccupation.Font = new System.Drawing.Font("Poppins", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DOccupation.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(68)))), ((int)(((byte)(76)))));
-            this.DOccupation.Location = new System.Drawing.Point(46, 395);
-            this.DOccupation.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.DOccupation.Name = "DOccupation";
-            this.DOccupation.Size = new System.Drawing.Size(188, 37);
-            this.DOccupation.TabIndex = 17;
-            this.DOccupation.Text = "Occupation Text";
-            this.DOccupation.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblEmail
             // 
@@ -100,23 +85,6 @@
             this.DEmailAddress.TabIndex = 14;
             this.DEmailAddress.Text = "Email  Address Text";
             this.DEmailAddress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // DFullName
-            // 
-            this.DFullName.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.DFullName.AutoSize = true;
-            this.DFullName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(2)))), ((int)(((byte)(34)))));
-            this.DFullName.Font = new System.Drawing.Font("Poppins SemiBold", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DFullName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(68)))), ((int)(((byte)(76)))));
-            this.DFullName.Location = new System.Drawing.Point(45, 212);
-            this.DFullName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.DFullName.Name = "DFullName";
-            this.DFullName.Size = new System.Drawing.Size(275, 48);
-            this.DFullName.TabIndex = 16;
-            this.DFullName.Text = "Display Name Text";
-            this.DFullName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label2
             // 
@@ -160,12 +128,68 @@
             this.LogoutButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
             this.LogoutButton.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LogoutButton.ForeColor = System.Drawing.Color.White;
-            this.LogoutButton.Location = new System.Drawing.Point(54, 459);
+            this.LogoutButton.Location = new System.Drawing.Point(276, 470);
             this.LogoutButton.Name = "LogoutButton";
             this.LogoutButton.Size = new System.Drawing.Size(129, 45);
             this.LogoutButton.TabIndex = 24;
             this.LogoutButton.Text = "Logout";
             this.LogoutButton.Click += new System.EventHandler(this.LogoutButton_Click);
+            // 
+            // DFullName
+            // 
+            this.DFullName.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.DFullName.DefaultText = "";
+            this.DFullName.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.DFullName.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.DFullName.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.DFullName.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.DFullName.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DFullName.Font = new System.Drawing.Font("Poppins SemiBold", 20.25F, System.Drawing.FontStyle.Bold);
+            this.DFullName.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DFullName.Location = new System.Drawing.Point(54, 206);
+            this.DFullName.Margin = new System.Windows.Forms.Padding(7, 10, 7, 10);
+            this.DFullName.Name = "DFullName";
+            this.DFullName.PasswordChar = '\0';
+            this.DFullName.PlaceholderText = "";
+            this.DFullName.SelectedText = "";
+            this.DFullName.Size = new System.Drawing.Size(360, 52);
+            this.DFullName.TabIndex = 25;
+            // 
+            // DOccupation
+            // 
+            this.DOccupation.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.DOccupation.DefaultText = "";
+            this.DOccupation.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.DOccupation.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.DOccupation.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.DOccupation.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.DOccupation.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DOccupation.Font = new System.Drawing.Font("Poppins", 15.75F);
+            this.DOccupation.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.DOccupation.Location = new System.Drawing.Point(54, 399);
+            this.DOccupation.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.DOccupation.Name = "DOccupation";
+            this.DOccupation.PasswordChar = '\0';
+            this.DOccupation.PlaceholderText = "";
+            this.DOccupation.SelectedText = "";
+            this.DOccupation.Size = new System.Drawing.Size(351, 50);
+            this.DOccupation.TabIndex = 26;
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.BorderRadius = 8;
+            this.SaveButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.SaveButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.SaveButton.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.SaveButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.SaveButton.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(209)))), ((int)(((byte)(172)))));
+            this.SaveButton.Font = new System.Drawing.Font("Poppins", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.ForeColor = System.Drawing.Color.White;
+            this.SaveButton.Location = new System.Drawing.Point(53, 470);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(198, 45);
+            this.SaveButton.TabIndex = 27;
+            this.SaveButton.Text = "Save Changes";
             // 
             // ProfileView
             // 
@@ -173,11 +197,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(462, 561);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.DOccupation);
+            this.Controls.Add(this.DFullName);
             this.Controls.Add(this.LogoutButton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.DOccupation);
-            this.Controls.Add(this.DFullName);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.DEmailAddress);
             this.Controls.Add(this.ProfilePicBox);
@@ -194,12 +219,13 @@
 
         #endregion
         private Guna.UI.WinForms.GunaCirclePictureBox ProfilePicBox;
-        private System.Windows.Forms.Label DOccupation;
         private System.Windows.Forms.Label lblEmail;
         private System.Windows.Forms.Label DEmailAddress;
-        private System.Windows.Forms.Label DFullName;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private Guna.UI2.WinForms.Guna2Button LogoutButton;
+        private Guna.UI2.WinForms.Guna2TextBox DFullName;
+        private Guna.UI2.WinForms.Guna2TextBox DOccupation;
+        private Guna.UI2.WinForms.Guna2Button SaveButton;
     }
 }
